b2377a1bcf5e0efccb48a47f12f47659ca61911d
julian3xl <julian3xl@gmail.com>
2023-01-30 15:04:33 +0100

    credentials wrapper added to launch processes that only admit credentials by env variables
    Signed-off-by: julian3xl <julian3xl@gmail.com>

--- run.sh
+++ run.sh
@@ -20,4 +20,11 @@ echo "[default]" > /tmp/credentials
 /root/.local/bin/crudini --set /tmp/credentials default aws_secret_access_key ${SECRET_ACCESS_KEY}
 /root/.local/bin/crudini --set /tmp/credentials default aws_session_token ${SESSION_TOKEN}
 
+echo "#!/bin/sh" > /tmp/credentials-wrapper.sh
+echo "export AWS_ACCESS_KEY_ID=${ACCESS_KEY_ID}" >> /tmp/credentials-wrapper.sh
+echo "export AWS_SECRET_ACCESS_KEY=${SECRET_ACCESS_KEY}" >> /tmp/credentials-wrapper.sh
+echo "export AWS_SESSION_TOKEN=${SESSION_TOKEN}" >> /tmp/credentials-wrapper.sh
+echo "exec \"\$@\"" >> /tmp/credentials-wrapper.sh
+chmod 755 /tmp/credentials-wrapper.sh
+
 cat /tmp/credentials
